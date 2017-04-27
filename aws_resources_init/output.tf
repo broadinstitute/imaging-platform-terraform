416e112f2f073a175496edc07fa6d37870cff5cd
karhohs <gandalfisarockstar@gmail.com>
2017-04-27 16:39:16 -0400

    add output to module

---
+++ aws_resources_init/output.tf
@@ -0,0 +1,17 @@
+output "iam_role_id" {
+
+  value = "${aws_iam_role.imaging-platform-terraform.id}"
+
+}
+
+output "sg_id" {
+
+  value = "${aws_security_group.imaging-platform-terraform-cellprofiler.id}"
+
+}
+
+output "subnet_id" {
+
+  value = "${aws_subnet.imaging-platform-terraform-cellprofiler.id}"
+
+}
